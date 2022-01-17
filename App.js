94210ba9322d6f1c1420d422f7075d1f1a5c9186
Lallu Mohan <92302444+lallumohan@users.noreply.github.com>
2022-01-17 12:05:58 +0530

    Add files via upload
    src files

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css'
+import './Login.js'
+import Login from './Form';
+import RegisterForm from './RegisterForm';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <RegisterForm/>
+    </div>
+  );
+}
+
+export default App;
